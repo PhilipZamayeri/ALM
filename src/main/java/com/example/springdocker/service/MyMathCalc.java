e90581bd5a3518f3de99051712edd70ffb2eafa9
Philip Zamayeri <Philip.Zamayeri@gmail.com>
2021-05-12 13:58:44 +0200

    Made a new class

---
+++ src/main/java/com/example/springdocker/service/MyMathCalc.java
@@ -0,0 +1,20 @@
+package com.example.springdocker.service;
+
+/**
+ * Created by Philip Zamayeri
+ * Date: 2021-05-12
+ * Time: 13:56
+ * Project: springdocker
+ * Copyright: MIT
+ */
+public class MyMathCalc {
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int multiply(int a, int b){
+        return a*b;
+    }
+    public int divide(int a, int b){
+        return a/b;
+    }
+}
